364752036901dc70a1fc5d8fdff6c422465b8f4a
MaglooLab <themagloolab@gmail.com>
2025-05-15 11:30:42 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
   headerTitle: 'Magloo Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Developing Solutions',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
